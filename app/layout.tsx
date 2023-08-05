5f57fadbfa9b9235bc46dfcdaf0c328b13ebce56
Styles <bcs_2021035@iiitm.ac.in>
2023-08-05 22:15:58 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Fit2Go',
-  description: 'Fit2Go',
+  description: 'A complete fitness app for everyone',
 }
 
 export default function RootLayout({
@@ -16,6 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
