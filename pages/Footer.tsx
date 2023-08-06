fd1d113be2989a2f02e8696cadd48128b565425a
Styles <bcs_2021035@iiitm.ac.in>
2023-08-07 00:39:48 +0530

    Update Footer.tsx

--- pages/Footer.tsx
+++ pages/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from "next/image"
 import logo_2 from "@/assets/logo_2.png"
+import logo from "@/assets/logo.svg"
 import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Footer = () => {
@@ -15,7 +16,7 @@ const Footer = () => {
                     <Image src={logo_2} alt="Fit2Go" className="object-cover w-8 p-1 rounded-full bg-primary aspect-square" />
                     <h1 className="text-2xl font-bold text-white">Fit2Go</h1>
                 </div>
-                <p className="text-[#BCBCBC] text-sm mt-4">In case of any queries, reach us at</p>
+                <p className="text-[#BCBCBC] text-sm mt-2">In case of any queries, reach us at</p>
                 <div className="bg-Slate mt-2 text-[#BCBCBC] p-10 flex flex-col border border-zinc-600">
                     <span>
                         <p className="font-bold text-primary">Phone:</p>
@@ -30,12 +31,10 @@ const Footer = () => {
                 </div>
             </div>
 
-            <form className="flex flex-col">
-                <h1 className="text-3xl font-bold tracking-wide text-primary">Connect with us</h1>
-                <input className="h-10 p-5 mt-3 bg-transparent border text-zinc-400 rounded-3xl" placeholder="Enter your email here" />
-                <button className="relative -top-[37px] left-[222px] flex items-center justify-center w-8 p-1 bg-primary aspect-square rounded-full"><AiOutlineArrowRight color="white" /></button>
-                <p className="hidden sm:block -mt-5 text-sm text-center text-zinc-400">Copyright © <span className="text-primary">Fit2Go 2023</span> </p>
-            </form>
+            <div className="hidden sm:flex flex-col justify-center">
+                <Image src={logo} alt="logo" className="mb-4"/>
+                <p className="text-sm text-center text-zinc-400">Copyright © <span className="text-primary">Fit2Go 2023</span> </p>
+            </div>
         </div>
     )
 }
