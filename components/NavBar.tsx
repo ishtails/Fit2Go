05e8ac2265fcbfc19645e630ed59b73b156fbeaa
Styles <bcs_2021035@iiitm.ac.in>
2023-08-06 22:30:08 +0530

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     <div className="z-0 flex items-center justify-between max-w-6xl py-5 mx-10 xl:mx-auto ">
       <Image src={logo} alt="logo" width={100} className="" />
 
-      <div className="items-center hidden space-x-8 font-medium sm:flex lg:space-x-14 text-slate-600">
+      <div className="items-center hidden space-x-4 font-medium sm:flex lg:space-x-14 text-slate-600">
         <Link
           to="objectives"
           spy={true}
@@ -18,20 +18,21 @@ const NavBar = () => {
           <p className="transition cursor-pointer hover:text-primary">About Us</p>
         </Link>
         <Link
-          to="services"
+          to="gallery"
           spy={true}
           smooth={true}
           offset={-50}
           duration={700}
         >
-          <p className="transition cursor-pointer hover:text-primary">Services</p>
+          <p className="transition cursor-pointer hover:text-primary">Gallery</p>
         </Link>
         <Link to="support" spy={true} smooth={true} offset={-10} duration={700}>
           <p className="transition cursor-pointer hover:text-primary">Contact</p>
         </Link>
       </div>
-      <Link to="support" spy={true} smooth={true} offset={-10} duration={700}>
-        <p className="px-4 py-1 text-white transition rounded-lg cursor-pointer bg-primary hover:animate-pulse">Get in touch</p>
+      
+      <Link to="services" spy={true} smooth={true} offset={-50} duration={700}>
+        <p className="font-medium px-4 py-1 text-white transition rounded-md cursor-pointer bg-primary hover:bg-[#10a58f]">Services</p>
       </Link>
     </div>
   );
