47ba665964f5b6f27f0afbf5ed8c61e832c69340
Styles <bcs_2021035@iiitm.ac.in>
2023-08-07 00:39:58 +0530

    Update Testimonials.tsx

--- pages/Testimonials.tsx
+++ pages/Testimonials.tsx
@@ -1,8 +1,13 @@
+'use client'
 import Testimonial from "@/components/Testimonial"
+import { LiaAngleRightSolid, LiaAngleLeftSolid } from "react-icons/lia"
+import React, { useRef } from "react";
 
-const Testimonials = ({testimonials}:{testimonials:any}) => {
+const Testimonials = ({ testimonials }: { testimonials: any }) => {
+    const testimonialsRef = useRef<HTMLDivElement>(null);
+    
     return (
-        <div id="testimonial" className="flex flex-col items-center h-full mx-10 mt-24 xl:mx-auto border-b-2 pb-4">
+        <div className="flex flex-col items-center h-full mx-10 mt-24 xl:mx-auto border-b-2 pb-4">
             <div className="flex flex-col items-center justify-center mb-10">
                 <h2 className="text-5xl font-bold text-center font-lexend xl:text-6xl">
                     Testimonials
@@ -12,7 +17,7 @@ const Testimonials = ({testimonials}:{testimonials:any}) => {
                 </span>
             </div>
 
-            <div className="max-w-[95vw] max-h-[75vh] flex flex-col md:flex-row overflow-x-scroll md:space-x-10 snap-x snap-mandatory xl:space-x-10 scrollbar-hide space-y-10 md:space-y-0 mb-5">
+            <div ref={testimonialsRef} className="max-w-[95vw] max-h-[75vh] flex flex-col md:flex-row overflow-x-scroll md:space-x-10 xl:space-x-10 scrollbar-hide space-y-10 md:space-y-0 mb-5 scroll-smooth">
                 {testimonials.map((testimonial: any) => (
                     <Testimonial
                         key={testimonial._id}
@@ -23,6 +28,24 @@ const Testimonials = ({testimonials}:{testimonials:any}) => {
                     />
                 ))}
             </div>
+
+            <div className="hidden md:flex space-x-8">
+                <button onClick={() => {
+                    if (testimonialsRef.current) {
+                        testimonialsRef.current.scrollLeft += 640;
+                    }
+                }} >
+                    <LiaAngleLeftSolid className="hover:text-primary transition-all" />
+                </button>
+                <button onClick={() => {
+                    if (testimonialsRef.current) {
+                        testimonialsRef.current.scrollLeft += 640;
+                    }
+                }} >
+                    <LiaAngleRightSolid className="hover:text-primary transition-all" />
+                </button>
+            </div>
+
         </div >
     )
 }
