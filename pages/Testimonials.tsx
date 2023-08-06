922da1a15bddf2fb7ed23aa30004ef3ab2df8ffb
Styles <bcs_2021035@iiitm.ac.in>
2023-08-06 11:55:43 +0530

    Update Testimonials.tsx

--- pages/Testimonials.tsx
+++ pages/Testimonials.tsx
@@ -1,18 +1,29 @@
 import Testimonial from "@/components/Testimonial"
 
-const Testimonials = () => {
+const Testimonials = ({testimonials}:{testimonials:any}) => {
     return (
-        <div id="testimonial" className="flex flex-col items-center h-full mx-10 xl:mx-auto mt-24">
-            <h2 className="text-center font-lexend font-bold text-4xl lg:text-5xl xl:text-6xl">
-                Testimonials
-            </h2>
-            
-            <div className="">
-                <Testimonial />
-                <Testimonial />
-                <Testimonial />
+        <div id="testimonial" className="flex flex-col items-center h-full mx-10 mt-24 xl:mx-auto">
+            <div className="flex flex-col items-center justify-center mb-10">
+                <h2 className="text-4xl font-bold text-center font-lexend lg:text-5xl xl:text-6xl">
+                    Testimonials
+                </h2>
+                <span className="justify-center mt-1 text-sm text-body lg:text-md xl:text-lg">
+                    Swipe to see what our users have to say
+                </span>
             </div>
-        </div>
+
+            <div className="max-w-[95vw] max-h-[75vh] flex flex-col md:flex-row overflow-x-scroll md:space-x-10 snap-x snap-mandatory xl:space-x-10 scrollbar-hide space-y-10 md:space-y-0 mb-5">
+                {testimonials.map((testimonial: any) => (
+                    <Testimonial
+                        key={testimonial._id}
+                        author={testimonial.author}
+                        designation={testimonial.designation}
+                        description={testimonial.description}
+                        imageURL={testimonial.imageURL}
+                    />
+                ))}
+            </div>
+        </div >
     )
 }
 
