385f08cc71cbe5b51e0933b17d0581f596ac908d
Styles <bcs_2021035@iiitm.ac.in>
2023-08-06 11:54:55 +0530

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,13 @@
+'use client'
 import Image from "next/image";
 import logo from "../assets/logo.svg";
 import { Link } from "react-scroll";
-import { useState } from "react";
 const NavBar = () => {
   return (
-    <div className="flex py-5 mx-10 justify-between max-w-6xl xl:mx-auto items-center z-0 ">
-      <Image src={logo} alt="logo" width={100} height={100} className="" />
+    <div className="z-0 flex items-center justify-between max-w-6xl py-5 mx-10 xl:mx-auto ">
+      <Image src={logo} alt="logo" width={100} className="" />
 
-      <div className="hidden sm:flex items-center space-x-8 lg:space-x-14 font-medium text-slate-600">
+      <div className="items-center hidden space-x-8 font-medium sm:flex lg:space-x-14 text-slate-600">
         <Link
           to="objectives"
           spy={true}
@@ -15,23 +15,23 @@ const NavBar = () => {
           offset={-50}
           duration={700}
         >
-          <p className="cursor-pointer hover:text-primary transition">About Us</p>
+          <p className="transition cursor-pointer hover:text-primary">About Us</p>
         </Link>
         <Link
-          to="reviews"
+          to="services"
           spy={true}
           smooth={true}
           offset={-100}
           duration={700}
         >
-          <p className="cursor-pointer hover:text-primary transition">Services</p>
+          <p className="transition cursor-pointer hover:text-primary">Services</p>
         </Link>
         <Link to="support" spy={true} smooth={true} offset={-10} duration={700}>
-          <p className="cursor-pointer hover:text-primary transition">Contact</p>
+          <p className="transition cursor-pointer hover:text-primary">Contact</p>
         </Link>
       </div>
       <Link to="support" spy={true} smooth={true} offset={-10} duration={700}>
-        <p className="cursor-pointer bg-primary py-1 px-4 text-white rounded-lg hover:animate-pulse transition">Get in touch</p>
+        <p className="px-4 py-1 text-white transition rounded-lg cursor-pointer bg-primary hover:animate-pulse">Get in touch</p>
       </Link>
     </div>
   );
