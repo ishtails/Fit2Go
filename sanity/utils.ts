27220a235f1fcccd09fd3e95049aca77d6795dc7
Styles <bcs_2021035@iiitm.ac.in>
2023-08-06 11:55:59 +0530

    Update utils.ts

--- sanity/utils.ts
+++ sanity/utils.ts
@@ -1,12 +1,14 @@
 import { createClient, groq } from "next-sanity";
 
-export async function getPets() {
+export async function getTestimonials() {
   const client = createClient({
     projectId: "o19642js",
     dataset: "production",
     apiVersion: "2023-08-04",
-    useCdn:true,
+    useCdn: true,
   });
 
-  return client.fetch(groq`*[_type == "pet"]{_id, name}`);
+  return client.fetch(
+    groq`*[_type == "testimonial"]{_id, author, designation, description, "imageURL": image.asset->url}`
+  );
 }
