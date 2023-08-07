596fb586b66308eb617fa2feed47bc53788d932b
Styles <bcs_2021035@iiitm.ac.in>
2023-08-08 01:58:18 +0530

    Update Team.tsx

--- pages/Team.tsx
+++ pages/Team.tsx
@@ -1,11 +1,22 @@
 'use client'
 import TeamCard from "@/components/TeamCard"
 import team_1 from "@/assets/Anush.jpeg"
-import { LiaAngleRightSolid, LiaAngleLeftSolid } from "react-icons/lia"
 import React, { useRef } from "react";
 import NavigationArrows from "@/components/NavigationArrows";
+import { urlFor } from "@/helpers/helpers";
 
-const Team = () => {
+type Props = {
+    array: {
+        _id: string,
+        name: string,
+        designation: string,
+        image: any,
+        linkedin?: string,
+        instagram?: string
+    }[],
+}
+
+const Team = ({ array }: Props) => {
     const teamRef = useRef<HTMLDivElement>(null);
     return (
         <div id="team" className="h-full flex flex-col max-w-6xl mx-10 sm:mt-10 xl:mx-auto">
@@ -16,15 +27,18 @@ const Team = () => {
                 Meet the team that makes it all possible
             </span>
             <div className="max-w-[95vw] max-h-[75vh] flex overflow-x-scroll space-x-10 scrollbar-hide bg-pale-green/10 mb-5 scroll-smooth rounded-xl p-5 snap-x snap-mandatory mt-10 sm:p-10" ref={teamRef}>
-                <TeamCard name="Anush Shukla" profession="Founder / Physiotherapist" image={team_1} linkedin="https://www.linkedin.com/in/anush-shukla-688729206/" instagram="https://www.instagram.com/tikks02/" />
-                <TeamCard name="Anush Shukla" profession="Founder / Physiotherapist" image={team_1} linkedin="https://www.linkedin.com/in/anush-shukla-688729206/" instagram="https://www.instagram.com/tikks02/" />
-                <TeamCard name="Anush Shukla" profession="Founder / Physiotherapist" image={team_1} linkedin="https://www.linkedin.com/in/anush-shukla-688729206/" instagram="https://www.instagram.com/tikks02/" />
-                <TeamCard name="Anush Shukla" profession="Founder / Physiotherapist" image={team_1} linkedin="https://www.linkedin.com/in/anush-shukla-688729206/" instagram="https://www.instagram.com/tikks02/" />
-                <TeamCard name="Anush Shukla" profession="Founder / Physiotherapist" image={team_1} linkedin="https://www.linkedin.com/in/anush-shukla-688729206/" instagram="https://www.instagram.com/tikks02/" />
-                <TeamCard name="Anush Shukla" profession="Founder / Physiotherapist" image={team_1} linkedin="https://www.linkedin.com/in/anush-shukla-688729206/" instagram="https://www.instagram.com/tikks02/" />
+                {array.map((member: any) => (
+                    <TeamCard 
+                    key={member._id} 
+                    name={member.name} 
+                    designation={member.designation} 
+                    imageURL={urlFor(member.image).width(600).height(600).url()}
+                    linkedin={member.linkedin}
+                    instagram={member.instagram} />
+                ))}
             </div>
 
-            <NavigationArrows scrollRef={teamRef} scrollBy={200}/>
+            <NavigationArrows scrollRef={teamRef} scrollBy={200} />
         </div>
     )
 }
