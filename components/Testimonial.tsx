56027b9604c379bbac5776351551de9139317c47
Styles <bcs_2021035@iiitm.ac.in>
2023-08-07 14:48:35 +0530

    fixed testimonials

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -12,15 +12,15 @@ type Props = {
 
 const Testimonial = ({ author, designation, description, imageURL }: Props) => {
     return (
-        <div className="bg-pale-green/20 py-10 sm:px-10 flex flex-col sm:flex-row justify-between sm:divide-x-2 space-x-2 items-center max-w-[640px] flex-shrink-0 snap-center rounded-xl divide-y-2 sm:divide-y-0">
-            <div className="relative pb-5 sm:pb-0 sm:pr-5">
+        <div className="bg-pale-green/20 py-10 md:px-10 flex flex-col md:flex-row md:justify-between md:divide-x-2 space-x-2 items-center max-w-[640px] flex-shrink-0 snap-center rounded-xl divide-y-2 md:divide-y-0">
+            <div className="relative pb-5 md:pb-0 md:pr-5">
                 <Image src={imageURL} width={120} height={120} alt="Testimonial Image" className="object-cover rounded-full aspect-square" />
                 <Image src={test_quote} width={40} alt="Testimonial Image" className="absolute p-3 rounded-full bg-primary -top-2 aspect-square" />
             </div>
-            <div className="sm:pl-10 w-[75%] pt-5 sm:pt-0">
+            <div className="md:pl-10 w-[75%] pt-5 md:pt-0">
                 <h3 className="text-xl font-semibold">{author}</h3>
                 <p className="text-sm">{designation}</p>
-                <p className="mt-2 text-sm text-zinc-700">{description}</p>
+                <p className="mt-2 max-w-[75vw] md:max-w-full text-sm text-zinc-700">{description}</p>
             </div>
         </div>
     )
